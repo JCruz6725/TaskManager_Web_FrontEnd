b294cc775abb3ed972cb822b9163c4bb8772791d
MuhammadJr <Muhammadou1drammeh@gmail.com>
2026-01-14 11:59:23 -0600

    login page created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,29 @@
 import { Routes } from '@angular/router';
 import { SignupComponent } from './components/sign-up/sign-up';
+import { LoginComponent } from './components/login/login';
 
 export const routes: Routes = [
+    {
+        path: 'login',
+        component: LoginComponent,
+        title: 'Login'
+    },
 
     {
-   path: 'signup',
-    component: SignupComponent,
-    title: 'Sign Up'
+        path: 'signup',
+        component: SignupComponent,
+        title: 'Sign Up'
     },
-  
+    {
+        path: '',
+        redirectTo: "login",
+        pathMatch: "full"
+    },
+
+    // {
+    //     path: '*',
+    //     redirectTo: 'login'
+    // }
+
 
 ];
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Inject, Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { RegisterUser } from "../models/register-user";
+import { LoginUser } from "../models/login-user";
 
 @Injectable({  providedIn: 'root'})
 export class UserAuthService {
@@ -8,10 +9,17 @@ export class UserAuthService {
  constructor(private http: HttpClient) {}
 private baseUrl = 'https://localhost:7177/'; 
 
-    registerUser(user: RegisterUser) {
-        return this.http.post(`${this.baseUrl}user`, user);
+    registerUser(userRegister: RegisterUser) {
+        return this.http.post(`${this.baseUrl}user`, userRegister);
     }
     
+    // loginUser(userLogin: { email: string; password: string }) {
+    //     return this.http.post(`${this.baseUrl}user/login`, userLogin);
+    // }
+
+     loginUser(userLogin: LoginUser){
+        return this.http.post(`${this.baseUrl}user/login`, userLogin);
+    }
 
 
 }
