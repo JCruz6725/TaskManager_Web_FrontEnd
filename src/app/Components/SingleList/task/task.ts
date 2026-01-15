39c74a16a589258b4cc8a6a65bde721e95dc6b8a
jacieylyn <jacquelinee.trann@yahoo.com>
2026-01-15 16:35:13 -0600

    Added abriged view of all tasks in lists

---
+++ src/app/Components/SingleList/task/task.ts
@@ -0,0 +1,25 @@
+import { Component, input, inject } from '@angular/core';
+import { TaskService } from '../../../Services/TaskServices/task-service';
+import { signal } from '@angular/core';
+import { DatePipe } from '@angular/common';
+
+@Component({
+  selector: 'app-task',
+  imports: [DatePipe],
+  templateUrl: './task.html',
+  styleUrl: './task.css',
+})
+export class Task {
+  apiService : TaskService = inject(TaskService);
+  taskId = input<string>("");
+
+  taskName = signal<string>("");
+  taskDueDate = signal<any>(null);
+
+  ngOnInit(){
+    this.apiService.getTask(this.taskId()).subscribe((res:any)=>{
+      this.taskName.set(res.title);
+      this.taskDueDate.set(new Date(res.dueDate));
+    });
+  }
+}
