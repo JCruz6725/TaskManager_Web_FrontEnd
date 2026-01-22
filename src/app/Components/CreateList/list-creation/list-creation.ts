70a4a1f65b3a426cd9ca696b002325c3f38d7c47
TerryRios <terryvaneijk55@yahoo.com>
2026-01-22 09:07:43 -0600

    Clone Text Box Destroyed Implemented and Automatic Cursor Placement inside Input(Text)

--- src/app/Components/CreateList/list-creation/list-creation.ts
+++ src/app/Components/CreateList/list-creation/list-creation.ts
@@ -1,5 +1,5 @@
 import { ListService } from './../../../Services/ListServiceAll/get-all-list';
-import { Component, EventEmitter, inject, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, inject, Input, OnInit, Output, ViewChild, ElementRef } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -17,11 +17,18 @@ export class ListCreation  {
   showErrorMessage: boolean = false;
   listservice=inject(ListService);
 
+  @ViewChild('listTitleInput') listTitleInput?: ElementRef<HTMLInputElement>;
+
   @Output ("getAllList")  getAllList: EventEmitter<any> = new EventEmitter();
+  // Destroys the clone field after creating the list
+  @Output() toggleInputChange = new EventEmitter<boolean>();
 
   postSingleList():void {
     if(!this.inputValue || !this.inputValue.trim()){
       this.showErrorMessage=true;
+      this.toggleInputChange.emit(true);
+      console.log(' Error : Input is empty');
+
       return;
     }
     this.showErrorMessage=false;
@@ -30,10 +37,13 @@ export class ListCreation  {
         this.listservice.CreateList(this.inputValue).subscribe((response) => {
           this.inputValue='';
           this.getAllList.emit();
+          this.toggleInputChange.emit(false);
         });
         // Creates List and refreshes the list in home component by calling getAllList function
     }
-  }
-
-
-
+    focusOnListTitle(): void {
+      if (this.listTitleInput?.nativeElement) {
+        this.listTitleInput.nativeElement.focus();
+      }
+    }
+}
--- src/app/Components/Home/home.ts
+++ src/app/Components/Home/home.ts
@@ -1,5 +1,5 @@
 import { ListService } from '../../Services/ListServiceAll/get-all-list';
-import { Component, signal, OnInit } from '@angular/core';
+import { Component, signal, OnInit, viewChild, ViewChild } from '@angular/core';
 import { Single } from '../SingleList/single/single';
 import { ListCreation } from '../CreateList/list-creation/list-creation';
 import { CommonModule } from '@angular/common';
@@ -14,16 +14,24 @@ import { CommonModule } from '@angular/common';
 export class Home implements OnInit {
   public userList = signal<any[]>([]);
 
+  @ViewChild(ListCreation) listCreation?: ListCreation;
+
   newList = false;
+
   toggleInput(): void {
     this.newList = !this.newList;
+    if (this.newList) {
+      // Focus on the input field when it becomes visible
+      setTimeout(() => {
+        this.listCreation?.focusOnListTitle();
+      }, );
+    }
   }
   constructor(private service: ListService) {}
 
   ngOnInit(): void {
     this.getAllList();
   }
-
   getAllList() {
     this.service.AllList().subscribe((result: any[]) => {
       this.userList.set(result);
