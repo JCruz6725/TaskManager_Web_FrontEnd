f855a2119cda180d9f740e5bf61eafe02ebfb984
MuhammadJr <Muhammadou1drammeh@gmail.com>
2026-01-23 16:59:51 -0600

    Delete note is functioning

--- src/app/Components/TaskDetails/view-notes/view-notes.ts
+++ src/app/Components/TaskDetails/view-notes/view-notes.ts
@@ -3,6 +3,7 @@ import { Component, inject, input, Input, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { DataSharingService } from '../../../Services/TaskServices/DataSharingService/data-sharing-service';
 import { MatIconModule } from '@angular/material/icon';
+import { NoteService } from '../../../Services/NoteServices/note-service';
 
 @Component({
   selector: 'app-view-notes',
@@ -12,24 +13,40 @@ import { MatIconModule } from '@angular/material/icon';
 })
 export class ViewNotes {
   private sharedSvc = inject(DataSharingService);
+  private noteSvc = inject(NoteService);
+
   public notes = signal<any[]>([]);
+  // public taskId = signal<string | null>(null)
+  public taskId = signal<string | null>(null)
+
 
   ngOnInit() {
     this.sharedSvc.currentChildData$.subscribe(task => {
-      console.log('Task in ViewNotes:', task);
+      // console.log('Task in ViewNotes:', task);
       if (!task || !task.notes) {
         this.notes.set([]);
         return;
       }
       this.notes.set(task.notes);
+      this.taskId.set(task.id)
     });
   }
 
-  DeleteNote(index: number) {
-    let currentNotes = this.notes();
-    let confirmDelet = confirm("Are you sure you want to delete this note?");
-    if (confirmDelet) {
-      currentNotes.splice(index, 1);
-    }
-  }
+   DeleteNote(noteId: string) {
+
+    console.log("note id " + noteId)
+    const taskId = this.taskId();
+    console.log(taskId)
+    if(!taskId)
+      return;
+     if(!confirm('Delete Note'))
+      return;
+     this.noteSvc.deleteNote(taskId, noteId)
+      .subscribe({
+        next: () => {
+          this.notes.update(n => n.filter(note => note.id !== noteId));
+        }
+      });
+     }
+
 }
---
+++ src/app/Services/NoteServices/note-service.ts
@@ -0,0 +1,37 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { UserTokenService } from '../UserToken/user-token.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class NoteService {
+  private http = inject(HttpClient);
+    private userIdTokenService = inject(UserTokenService);
+  
+  // private baseUrl = 'https://localhost:7177/notes';
+   
+  // createNote(taskId: string, note: string) {
+  //   return this.http.post<any>(this.baseUrl + taskId, {}, { headers: this.makeApiHeader() });
+  // }
+
+  createNote(taskId: string, noteText: string){
+    const url =  `https://localhost:7177/Task/${taskId}/notes`;
+   return this.http.post<any>(url,
+       { noteText }, { headers: this.makeApiHeader() });
+  } 
+
+      deleteNote(taskId : string, noteId: string ){
+      const url = `https://localhost:7177/Task/${taskId}/notes/${noteId}`
+         return this.http.delete<any>(url,
+            { headers: this.makeApiHeader() });
+    }
+
+  private makeApiHeader(): HttpHeaders {
+    return new HttpHeaders({
+      UserId: this.userIdTokenService.GetUserIdToken() ?? (() => { throw new Error("User ID Token is null"); })()
+    });
+  }
+
+
+}
