7835ee9bcb9cf02b2ca56b8dad2462a0fe432783
jacieylyn <jacquelinee.trann@yahoo.com>
2026-03-12 13:07:42 -0500

    updated submit button and added error messages

--- src/app/Components/reset-password/reset-password.ts
+++ src/app/Components/reset-password/reset-password.ts
@@ -5,15 +5,18 @@ import { UserAuthService } from '../../Services/auth/auth.service';
 import { HttpErrorResponse } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-reset-password',
-  imports: [FormsModule, CommonModule, RouterLink],
+  imports: [FormsModule, CommonModule, RouterLink, MatButton],
   templateUrl: './reset-password.html',
   styleUrl: './reset-password.css',
 })
 export class ResetPasswordComponent {
   userSvc : UserAuthService = inject(UserAuthService);
+
+  errorMessage = signal<string | null>(null);
   successMessage = signal<string | null>(null);
 
   model: ResetPassword = {
@@ -24,17 +27,16 @@ export class ResetPasswordComponent {
 
   submitForm(form: any){
     this.successMessage.set(null);
+    this.errorMessage.set(null);
 
     this.userSvc.resetPassword(this.model).subscribe({
       next: (response) => {
         this.successMessage.set("Successfully Changed Password")
       },
       error: (err: HttpErrorResponse) => {
-        console.log(err.error);
+        this.errorMessage.set(err.error);
       }
     })
   }
-
-
 
 }
